4e1234d641580a36e8fb42b4d5071380ba2fa512
Mohib Ismail <imohib168@gmail.com>
2020-11-16 21:28:07 +0500

    Completed 9 projects

---
+++ Sound Borad/script.js
@@ -0,0 +1,14 @@
+const sounds = ["Applause", "Boo", "Gasp", "Tada", "Victory", "Wrong"];
+
+sounds.forEach((sound) => {
+    const btn = document.createElement("button");
+    btn.classList.add("btn");
+
+    btn.innerText = sound;
+
+    btn.addEventListener("click", () => {
+        document.getElementById(sound).play();
+    })
+
+    document.body.appendChild(btn);
+})
